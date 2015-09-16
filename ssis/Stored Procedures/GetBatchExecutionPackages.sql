8bd7bdd76753e7543c8607fbab1ac14f53840fa4
avenantp <peter.avenant@varigence.com.au>
2015-09-16 13:44:05 +1000

    Fix some minor reporting issues

--- ssis/Stored Procedures/GetBatchExecutionPackages.sql
+++ ssis/Stored Procedures/GetBatchExecutionPackages.sql
@@ -3,46 +3,43 @@
 	@ExecutionID VARCHAR(30)
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-SELECT e.ExecutionID
-	,e.ParentExecutionID
-	,e.ServerExecutionID
-	,e.ParentSourceGUID
-	,e.ExecutionGUID
-	,e.SourceGUID
-	,e.PackageID
-	,p.PackageName
-	,e.ExecutionStatus
-	,e.NextLoadStatus
-	,CONVERT(DATETIME, e.StartTime) AS StartTime
-	,CONVERT(DATETIME, e.EndTime) AS EndTime
-	,RIGHT('0' + CONVERT(varchar(5)
-	,DATEDIFF(s, e.StartTime, e.EndTime) / 3600), 2) + ':' + RIGHT('0' + CONVERT(VARCHAR(5), DATEDIFF(s, e.StartTime, e.EndTime) % 3600 / 60), 2) + ':' + RIGHT('0' + CONVERT(varchar(5), DATEDIFF(s, e.StartTime, e.EndTime) % 60), 2) AS Duration
-	,SUM(CASE WHEN rc.[CountType] = 'Select' THEN rc.[RowCount] END) AS [RowCount]
-	,SUM(ar.[RowCount]) AS AuditTypeRowCount
-FROM ssis.Execution AS e
-INNER JOIN 
-	ssis.Package AS p ON e.PackageID = p.PackageID 
-LEFT OUTER JOIN
-	ssis.[RowCount] AS rc ON e.ExecutionID = rc.ExecutionID 
-LEFT OUTER JOIN
-	ssis.AuditRow AS ar ON e.ExecutionID = ar.ExecutionID
-WHERE e.ExecutionID = @ExecutionID
-	--AND e.ParentExecutionID <> -1
+SET NOCOUNT ON;
+SELECT	 e.ExecutionID
+		,e.ParentExecutionID
+		,e.ServerExecutionID
+		,e.ParentSourceGUID
+		,e.ExecutionGUID
+		,e.SourceGUID
+		,e.PackageID
+		,p.PackageName
+		,e.ExecutionStatus
+		,e.NextLoadStatus
+		,CONVERT(DATETIME, e.StartTime) AS StartTime
+		,CONVERT(DATETIME, e.EndTime) AS EndTime
+		,RIGHT('0' + CONVERT(varchar(5)
+		,DATEDIFF(s, e.StartTime, e.EndTime) / 3600), 2) + ':' + RIGHT('0' + CONVERT(VARCHAR(5), DATEDIFF(s, e.StartTime, e.EndTime) % 3600 / 60), 2) + ':' + RIGHT('0' + CONVERT(varchar(5), DATEDIFF(s, e.StartTime, e.EndTime) % 60), 2) AS Duration
+		,SUM(CASE WHEN rc.[CountType] = 'Select' THEN rc.[RowCount] END) AS [RowCount]
+		,SUM(ar.[RowCount]) AS AuditTypeRowCount
+FROM [ssis].[Execution] AS e
+INNER JOIN [ssis].[Package] AS p 
+	ON	e.PackageID = p.PackageID 
+LEFT OUTER JOIN [ssis].[RowCount] AS rc 
+	ON	e.ExecutionID = rc.ExecutionID 
+LEFT OUTER JOIN [ssis].[AuditRow] AS ar 
+	ON	e.ExecutionID = ar.ExecutionID
+WHERE	e.ExecutionID = @ExecutionID
 GROUP BY e.ExecutionID
-	,e.ParentExecutionID
-	,e.ServerExecutionID
-	,e.ParentSourceGUID
-	,e.ExecutionGUID
-	,e.SourceGUID 
-	,e.PackageID
-	,p.PackageName
-	,e.ExecutionStatus
-	,e.NextLoadStatus
-	,e.StartTime
-	,e.EndTime
+		,e.ParentExecutionID
+		,e.ServerExecutionID
+		,e.ParentSourceGUID
+		,e.ExecutionGUID
+		,e.SourceGUID 
+		,e.PackageID
+		,p.PackageName
+		,e.ExecutionStatus
+		,e.NextLoadStatus
+		,e.StartTime
+		,e.EndTime
 
 RETURN 0
 END
--- ssis/Stored Procedures/GetExecutionErrors.sql
+++ ssis/Stored Procedures/GetExecutionErrors.sql
@@ -1,20 +1,19 @@
-﻿CREATE PROCEDURE [dbo].[GetExecutionErrors]
+﻿CREATE PROCEDURE [ssis].[GetExecutionErrors]
 	-- Add the parameters for the stored procedure here
 	@ExecutionID VARCHAR(25)
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
 	SELECT ee.[ExecutionErrorID]
-		  ,ee.[PackageID]
-		  ,p.[PackageName]
-		  ,ee.[ExecutionID]
-		  ,ee.[ErrorCode]
-		  ,ee.[ErrorDescription] 
-	FROM [BimlCatalog].[ssis].[ExecutionError] ee
-	INNER JOIN [BimlCatalog].[ssis].[Package] p ON ee.[PackageID] = p.[PackageID]
-	WHERE ee.[ExecutionID] = @ExecutionID
+			,ee.[PackageID]
+			,p.[PackageName]
+			,ee.[ExecutionID]
+			,ee.[ErrorCode]
+			,ee.[ErrorDescription] 
+	FROM [ssis].[ExecutionError] ee
+	INNER JOIN [ssis].[Package] p 
+		ON ee.[PackageID] = p.[PackageID]
+	WHERE	ee.[ExecutionID] = @ExecutionID
 
 END
