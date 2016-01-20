9c2195aa055b6d03ad3556c86b930474c41fb4f2
avenantp <peter.avenant@varigence.com.au>
2016-01-20 22:33:02 +1100

    Add ORACLE Date conversion on import and SolveOrder fix

---
+++ dbo/Tables/sysssislog.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[sysssislog] (
+		[id]          INT              IDENTITY (1, 1) NOT NULL,
+		[event]       [sysname]        NOT NULL,
+		[computer]    NVARCHAR (128)   NOT NULL,
+		[operator]    NVARCHAR (128)   NOT NULL,
+		[source]      NVARCHAR (1024)  NOT NULL,
+		[sourceid]    UNIQUEIDENTIFIER NOT NULL,
+		[executionid] UNIQUEIDENTIFIER NOT NULL,
+		[starttime]   DATETIME         NOT NULL,
+		[endtime]     DATETIME         NOT NULL,
+		[datacode]    INT              NOT NULL,
+		[databytes]   IMAGE            NULL,
+		[message]     NVARCHAR (2048)  NOT NULL,
+		PRIMARY KEY CLUSTERED ([id] ASC)
+	);
