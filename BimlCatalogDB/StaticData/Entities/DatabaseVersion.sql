19c7479ec3308411bfcdb3234924397522b6b87e
avenantp <peter.avenant@varigence.com.au>
2018-04-17 11:03:39 +1000

    CheckPoint for Bundle 62817

--- BimlCatalogDB/StaticData/Entities/DatabaseVersion.sql
+++ BimlCatalogDB/StaticData/Entities/DatabaseVersion.sql
@@ -1,1 +1,1 @@
-UPDATE [admin].[Configurations] SET [ConfigurationValue] = '62803' WHERE [ConfigurationCode] = 'BimlCatalog' AND [ConfigurationKey] = 'DatabaseVersion' 
+UPDATE [admin].[Configurations] SET [ConfigurationValue] = '62817' WHERE [ConfigurationCode] = 'BimlCatalog' AND [ConfigurationKey] = 'DatabaseVersion' 
