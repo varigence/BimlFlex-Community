2be1613a307bd37d0dca19d8aef7955a0d9c633c
avenantp <peter.avenant@varigence.com.au>
2018-10-26 06:02:22 +1100

    Release Dacpac Checkpoint 63426

--- BimlCatalogDB/StaticData/Entities/DatabaseVersion.sql
+++ BimlCatalogDB/StaticData/Entities/DatabaseVersion.sql
@@ -1,1 +1,1 @@
-UPDATE [admin].[Configurations] SET [ConfigurationValue] = '63416' WHERE [ConfigurationCode] = 'BimlCatalog' AND [ConfigurationKey] = 'DatabaseVersion' 
+UPDATE [admin].[Configurations] SET [ConfigurationValue] = '63426' WHERE [ConfigurationCode] = 'BimlCatalog' AND [ConfigurationKey] = 'DatabaseVersion' 
